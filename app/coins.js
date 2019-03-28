8c7eed5a9a801bfc3f01965a3fc9fa0846a1571f
TheTrunk <tadeas.kmenta@zel.cash>
2019-03-28 21:52:58 +0100

    bzx - coinbasetransaction is not correct as well as pools

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,11 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
+var bzx = require("./coins/bzx.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
+	"BZX": bzx,
 
-	"coins":["BTC", "LTC"]
+	"coins":["BTC", "LTC", "BZX"]
 };
