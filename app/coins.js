2159b2e5059bb0144da2b6be32a59ee040f14008
TheTrunk <tadeas.kmenta@zel.cash>
2019-03-30 15:04:15 +0100

    grs

--- app/coins.js
+++ app/coins.js
@@ -1,11 +1,13 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
 var bzx = require("./coins/bzx.js");
+var bzx = require("./coins/bzx.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
 	"BZX": bzx,
-
-	"coins":["BTC", "LTC", "BZX"]
+	"GRS": grs
+,
+	"coins":["BTC", "LTC", "BZX", "GRS"]
 };
---
+++ app/coins/grs.js
@@ -0,0 +1,122 @@
+var Decimal = require("decimal.js");
+Decimal8 = Decimal.clone({ precision:8, rounding:8 });
+
+var currencyUnits = [
+	{
+		type:"GRS",
+		name:"GRS",
+		multiplier:1,
+		default:true,
+		values:["", "grs", "GRS"],
+		decimalPlaces:8
+	},
+	{
+		type:"native",
+		name:"mGRS",
+		multiplier:1000,
+		values:["mgrs"],
+		decimalPlaces:5
+	},
+	{
+		type:"native",
+		name:"bits",
+		multiplier:1000000,
+		values:["bits"],
+		decimalPlaces:2
+	},
+	{
+		type:"native",
+		name:"sat",
+		multiplier:100000000,
+		values:["sat", "satoshi"],
+		decimalPlaces:0
+	},
+	{
+		type:"exchanged",
+		name:"USD",
+		multiplier:"usd",
+		values:["usd"],
+		decimalPlaces:2,
+		symbol:"$"
+	},
+];
+
+module.exports = {
+	name:"Groestlcoin",
+	ticker:"GRS",
+	logoUrl:"/img/logo/grs.svg",
+	siteTitle:"Groestlcoin Explorer",
+	nodeTitle:"Groestlcoin Full Node",
+	nodeUrl:"https://bitcoinzerox.net/",
+	demoSiteUrl: "https://ltc.chaintools.io",
+	miningPoolsConfigUrls:[
+		"https://raw.githubusercontent.com/hashstream/pools/master/pools.json",
+	],
+	maxBlockWeight: 40000000,
+	targetBlockTimeSeconds: 150,
+	currencyUnits:currencyUnits,
+	currencyUnitsByName:{"GRS":currencyUnits[0], "mGRS":currencyUnits[1], "bits":currencyUnits[2], "sat":currencyUnits[3]},
+	baseCurrencyUnit:currencyUnits[3],
+	defaultCurrencyUnit:currencyUnits[0],
+	feeSatoshiPerByteBucketMaxima: [5, 10, 25, 50, 100, 150, 200, 250],
+	genesisBlockHash: "00000ac5927c594d49cc0bdb81759d0da8297eb614683d3acb62f0703b639023",
+	genesisCoinbaseTransactionId: "3ce968df58f9c8a752306c4b7264afab93149dbc578bd08a42c446caaa6628bb",
+	genesisCoinbaseTransaction: {
+		"txid":"3ce968df58f9c8a752306c4b7264afab93149dbc578bd08a42c446caaa6628bb",
+		"hash":"3ce968df58f9c8a752306c4b7264afab93149dbc578bd08a42c446caaa6628bb",
+		"blockhash":"00000ac5927c594d49cc0bdb81759d0da8297eb614683d3acb62f0703b639023",
+		"version":2,
+		"locktime":0,
+		"size":168,
+		"vsize":168,
+		"time":1395342829,
+		"blocktime":1395342829,
+		"vin":[
+			{
+				"prev_out":{
+					"hash":"0000000000000000000000000000000000000000000000000000000000000000",
+					"n":4294967295
+				},
+				"coinbase":"04ffff001d0104404e592054696d65732030352f4f63742f32303131205374657665204a6f62732c204170706c65e280997320566973696f6e6172792c2044696573206174203536"
+			}
+		],
+		"vout":[
+			{
+				"value":"50.00000000",
+				"n":0,
+				"scriptPubKey":{
+					"hex":"040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9 OP_CHECKSIG",
+					"type":"pubkey",
+					"reqSigs":1,
+					"addresses":[
+						"Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2"
+					]
+				}
+			}
+		]
+	},
+	historicalData: [
+	],
+	exchangeRateData:{
+		jsonUrl:"https://api.coinmarketcap.com/v1/ticker/groestlcoin/",
+		exchangedCurrencyName:"usd",
+		responseBodySelectorFunction:function(responseBody) {
+			if (responseBody[0] && responseBody[0].price_usd) {
+				return {"usd":responseBody[0].price_usd};
+			}
+			
+			return null;
+		}
+	},
+	blockRewardFunction:function(blockHeight) {
+		var eras = [ new Decimal8(75) ];
+		for (var i = 1; i < 34; i++) {
+			var previous = eras[i - 1];
+			eras.push(new Decimal8(previous).dividedBy(2));
+		}
+
+		var index = Math.floor(blockHeight / 840000);
+
+		return eras[index];
+	}
+};
