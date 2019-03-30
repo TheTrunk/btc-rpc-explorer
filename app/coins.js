50947b3c23d9661dc61cb052675e8d9f166cf590
TheTrunk <tadeas.kmenta@zel.cash>
2019-03-30 15:14:03 +0100

    fix grs

--- app/coins.js
+++ app/coins.js
@@ -1,7 +1,7 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
 var bzx = require("./coins/bzx.js");
-var bzx = require("./coins/bzx.js");
+var grs = require("./coins/grs.js");
 
 module.exports = {
 	"BTC": btc,
