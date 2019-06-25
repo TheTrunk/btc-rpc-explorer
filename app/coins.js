aac2b188e17663a6dda931e7493ad3936f47c4df
TheTrunk <tadeas.kmenta@zel.cash>
2019-06-25 09:28:08 +0200

    doge

--- app/coins.js
+++ app/coins.js
@@ -2,12 +2,14 @@ var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
 var bzx = require("./coins/bzx.js");
 var grs = require("./coins/grs.js");
+var doge = require("./coins/doge.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
 	"BZX": bzx,
-	"GRS": grs
+	"GRS": grs,
+	"DOGE": doge
 ,
-	"coins":["BTC", "LTC", "BZX", "GRS"]
+	"coins":["BTC", "LTC", "BZX", "GRS", "DOGE"]
 };
